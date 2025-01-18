29096a4bc02846d9b614b8b8523763fe8807f15d
nirvan <nirvan.work@gmail.com>
2025-01-18 14:07:05 +0530

    add web server impl

---
+++ internal/server/webServer.go
@@ -0,0 +1,59 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"sync"
+
+	"github.com/IamNirvan/veritasengine/internal/handlers"
+	"github.com/IamNirvan/veritasengine/internal/services/config"
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+type WebServer struct {
+	Config   *config.Config
+	Handlers *handlers.Handlers
+	Server   *http.Server
+}
+
+var (
+	instance *WebServer
+	once     sync.Once
+)
+
+type WebServerOptions struct {
+	Config   *config.Config
+	Handlers *handlers.Handlers
+}
+
+func NewWebServer(opts *WebServerOptions) *WebServer {
+	once.Do(func() {
+		instance = &WebServer{
+			Config:   (*opts).Config,
+			Handlers: (*opts).Handlers,
+		}
+	})
+	return instance
+}
+
+func (ws *WebServer) Start() error {
+	log.Debug("starting web server")
+
+	r := gin.Default()
+
+	r.POST("/v1/evaluate/rule", (*ws.Handlers.RuleEvaluationHandler).EvaluateRule)
+
+	ws.Server = &http.Server{
+		Addr:    fmt.Sprintf("%s:%d", ws.Config.WebServer.Host, ws.Config.WebServer.Port),
+		Handler: r,
+	}
+
+	return ws.Server.ListenAndServe()
+}
+
+func (ws *WebServer) Dispose(ctx context.Context) error {
+	log.Debug("stopping web server")
+	return ws.Server.Shutdown(ctx)
+}
